a9dfdeaa06fab752acecb6a7b100e04daf8adec5
Luis Schneiders <schneiders.tech@gmail.com>
2021-11-17 07:38:23 +1100

    add library Department

---
+++ DepartmentLibrary/Department.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DepartmentLibrary
+{
+    public class Department
+    {
+        private string _departmentID;
+        private string _departmentName;
+
+        // constructor
+        public Department()
+        {
+        }
+
+        // method Save
+        public void Save(string departmentName)
+        {
+            // Add validation for fields
+
+            DepartmentId = Guid.NewGuid().ToString();
+            DepartmentName = departmentName;
+            
+        }
+
+        public string DepartmentId
+        {
+            get => _departmentID;
+            set => _departmentID = value;
+        }
+
+        public string DepartmentName
+        {
+            get => _departmentName;
+            set => _departmentName = value;
+        }
+
+    }
+}
