d9f1b54dc49e21b94836afef8cdb1ec1a104dcb6
Luis Schneiders <schneiders.tech@gmail.com>
2021-11-19 08:21:29 +1100

    add ContacDetails library

---
+++ ContactDetailsLibrary/ContactDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ContactDetailsLibrary
+{
+    public class ContactDetails
+    {
+        private string _email;
+        private int _mobilePhone;
+        private int _phone;
+
+        // constructor
+        public ContactDetails()
+        {
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public int MobilePhone
+        {
+            get => _mobilePhone;
+            set => _mobilePhone = value;
+        }
+
+        public int Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+    }
+}
