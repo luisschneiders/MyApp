04ded087bf6b534c6f37db3e8934a06c3a45b58a
Luis Schneiders <schneiders.tech@gmail.com>
2021-11-19 11:18:55 +1100

    update type for field email and phone to type List<>

--- ContactDetailsLibrary/ContactDetails.cs
+++ ContactDetailsLibrary/ContactDetails.cs
@@ -1,34 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ContactDetailsLibrary
 {
     public class ContactDetails
     {
-        private string _email;
-        private int _mobilePhone;
-        private int _phone;
+        private List<string> _email;
+        private List<int> _phone;
+        private int _mobile;
 
         // constructor
         public ContactDetails()
         {
         }
 
-        public string Email
+        public List<string> Email
         {
             get => _email;
             set => _email = value;
         }
 
-        public int MobilePhone
-        {
-            get => _mobilePhone;
-            set => _mobilePhone = value;
-        }
-
-        public int Phone
+        public List<int> Phone
         {
             get => _phone;
             set => _phone = value;
         }
+
+        public int Mobile
+        {
+            get => _mobile;
+            set => _mobile = value;
+        }
+
     }
 }
