78dbfd27e89f2d63681a40fbf44f6ab407c0a690
Luis Schneiders <schneiders.tech@gmail.com>
2021-11-22 17:06:15 +1100

    add BaseLibrary

---
+++ BaseLibrary/Base.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BaseLibrary
+{
+    public abstract class Base
+    {
+        private DateTime? _createdAt;
+        private DateTime? _updatedAt;
+
+        // contructor
+        public Base()
+        {
+        }
+
+        public DateTime? CreatedAt
+        {
+            get => _createdAt;
+            set => _createdAt = value;
+        }
+
+        public DateTime? UpdatedAt
+        {
+            get => _updatedAt;
+            set => _updatedAt = value;
+        }
+
+    }
+}
