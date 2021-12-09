368fd01fc05d29ff003c07fa53b2d8c2c077b603
Luis Schneiders <schneiders.tech@gmail.com>
2021-12-09 18:36:49 +1100

    add DbSet Customer; add CustomerService

--- HealthCareApp/Data/ApplicationDbContext.cs
+++ HealthCareApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CustomerLibrary;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace HealthCareApp.Data
             : base(options)
         {
         }
+
+        public DbSet<Customer> Customers { get; set; }
     }
 }
---
+++ HealthCareApp/Data/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CustomerLibrary;
+
+namespace HealthCareApp.Data
+{
+    public class CustomerService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        // Constructor
+        public CustomerService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+
+        // async method to get list of customers
+        public async Task<List<Customer>> GetCustomersAsync()
+        {
+            return await _applicationDbContext.Customers.ToListAsync();
+        }
+
+        // async method to add new customer
+        public async Task<Customer> AddCustomerAsync(Customer customer)
+        {
+            try
+            {
+                _applicationDbContext.Customers.Add(customer);
+                await _applicationDbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return customer;
+        }
+    }
+}
