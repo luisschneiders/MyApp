5b25d71146d868e5363820f874a6e64146682e7c
Luis Schneiders <schneiders.tech@gmail.com>
2021-12-14 10:53:44 +1100

    update data migrations

---
+++ HealthCareApp/Data/Migrations/20211213085518_CreateSupplierSchema.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthCareApp.Data.Migrations
+{
+    public partial class CreateSupplierSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_ContactDetails_CustomerContactDetailsId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Location_CustomerLocationId",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerLocationId",
+                table: "Customers",
+                newName: "LocationId");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerContactDetailsId",
+                table: "Customers",
+                newName: "ContactDetailsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_CustomerLocationId",
+                table: "Customers",
+                newName: "IX_Customers_LocationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_CustomerContactDetailsId",
+                table: "Customers",
+                newName: "IX_Customers_ContactDetailsId");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Customers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "BOOLEAN");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "BOOLEAN");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "LockoutEnabled",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "BOOLEAN");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmailConfirmed",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "BOOLEAN");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_ContactDetails_ContactDetailsId",
+                table: "Customers",
+                column: "ContactDetailsId",
+                principalTable: "ContactDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Location_LocationId",
+                table: "Customers",
+                column: "LocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_ContactDetails_ContactDetailsId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Location_LocationId",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "LocationId",
+                table: "Customers",
+                newName: "CustomerLocationId");
+
+            migrationBuilder.RenameColumn(
+                name: "ContactDetailsId",
+                table: "Customers",
+                newName: "CustomerContactDetailsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_LocationId",
+                table: "Customers",
+                newName: "IX_Customers_CustomerLocationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_ContactDetailsId",
+                table: "Customers",
+                newName: "IX_Customers_CustomerContactDetailsId");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Customers",
+                type: "BOOLEAN",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "AspNetUsers",
+                type: "BOOLEAN",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "LockoutEnabled",
+                table: "AspNetUsers",
+                type: "BOOLEAN",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmailConfirmed",
+                table: "AspNetUsers",
+                type: "BOOLEAN",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_ContactDetails_CustomerContactDetailsId",
+                table: "Customers",
+                column: "CustomerContactDetailsId",
+                principalTable: "ContactDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Location_CustomerLocationId",
+                table: "Customers",
+                column: "CustomerLocationId",
+                principalTable: "Location",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ HealthCareApp/Data/Migrations/20211213233655_CreateSupplierSchema2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HealthCareApp.Data.Migrations
+{
+    public partial class CreateSupplierSchema2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SupplierId",
+                table: "Location",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SupplierId",
+                table: "ContactDetails",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SupplierName = table.Column<string>(type: "TEXT", nullable: true),
+                    SupplierABN = table.Column<string>(type: "TEXT", nullable: true),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    InsertedBy = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Location_SupplierId",
+                table: "Location",
+                column: "SupplierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactDetails_SupplierId",
+                table: "ContactDetails",
+                column: "SupplierId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContactDetails_Suppliers_SupplierId",
+                table: "ContactDetails",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Location_Suppliers_SupplierId",
+                table: "Location",
+                column: "SupplierId",
+                principalTable: "Suppliers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContactDetails_Suppliers_SupplierId",
+                table: "ContactDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Location_Suppliers_SupplierId",
+                table: "Location");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Location_SupplierId",
+                table: "Location");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ContactDetails_SupplierId",
+                table: "ContactDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "Location");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierId",
+                table: "ContactDetails");
+        }
+    }
+}
