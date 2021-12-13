1f669124816047c3ca6237c655547bcbdbb5f91b
Luis Schneiders <schneiders.tech@gmail.com>
2021-12-13 13:37:47 +1100

    update method AddCustomerAsync and constructor CustomerService;

--- HealthCareApp/Data/CustomerService.cs
+++ HealthCareApp/Data/CustomerService.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CustomerLibrary;
+using Microsoft.AspNetCore.Http;
 
 namespace HealthCareApp.Data
 {
     public class CustomerService
     {
         private readonly ApplicationDbContext _applicationDbContext;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         // Constructor
-        public CustomerService(ApplicationDbContext applicationDbContext)
+        public CustomerService(ApplicationDbContext applicationDbContext, IHttpContextAccessor httpContextAccessor)
         {
             _applicationDbContext = applicationDbContext;
+            _httpContextAccessor = httpContextAccessor;
         }
 
 
@@ -26,6 +29,14 @@ namespace HealthCareApp.Data
         // async method to add new customer
         public async Task<Customer> AddCustomerAsync(Customer customer)
         {
+
+            UserService userService = new UserService(_httpContextAccessor);
+
+            customer.IsActive = true;
+            customer.CreatedAt = customer.UpdatedAt  = DateTime.UtcNow;
+            // Get current logged user ID
+            customer.InsertedBy = userService.UserId();
+
             try
             {
                 _applicationDbContext.Customers.Add(customer);
