f13d6b6435f869b09c727b8edde6c739a5cd287e
Luis Schneiders <schneiders.tech@gmail.com>
2021-12-13 13:35:56 +1100

    create UserService to get current user logged in

---
+++ HealthCareApp/Data/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace HealthCareApp.Data
+{
+    public class UserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        // constructor
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid UserId()
+        {
+
+            var principal = _httpContextAccessor.HttpContext.User;
+            var loggedInUserId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return new Guid(loggedInUserId);
+
+        }
+    }
+}
