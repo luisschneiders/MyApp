ae54fcdbf382fc93ebed35843caab897748d0a71
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-12 09:50:06 +1100

    add enum Theme

---
+++ HealthCareApp/Settings/Theme/Theme.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HealthCareApp.Settings.Theme
+{
+    public enum Theme
+    {
+        app,
+        employee,
+        department,
+        patient,
+        supplier
+    }
+}
