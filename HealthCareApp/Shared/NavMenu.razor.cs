5ab911773597bbfe7ed22fc72d5773c329e4b4ad
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-18 07:48:42 +1100

    refactor NavMenu component

---
+++ HealthCareApp/Shared/NavMenu.razor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace HealthCareApp.Shared
+{
+    public partial class NavMenu
+    {
+        private enum NavSubmenu
+        {
+            None,
+            First,
+            Second
+        };
+
+        private bool collapseNavMenu = true;
+        private bool collapseNavSubmenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+        private string NavSubmenuIcon => collapseNavSubmenu ? "bi-chevron-expand" : "bi-chevron-contract";
+        private NavSubmenu navSubmenu = NavSubmenu.None;
+
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        private void ToggleNavSubmenu(NavSubmenu submenu)
+        {
+            collapseNavSubmenu = !collapseNavSubmenu;
+
+            if (navSubmenu == submenu)
+            {
+                navSubmenu = NavSubmenu.None;
+            }
+            else
+            {
+                navSubmenu = submenu;
+            }
+        }
+    }
+}
