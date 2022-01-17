dd52ae43b51f9a609958ce97b1c0ae4447b9c5d1
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-18 07:37:02 +1100

    refactor component Message

---
+++ HealthCareApp/Components/Message/MessageView.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.Message
+{
+    public partial class MessageView
+    {
+        [Parameter]
+        public string Icon { get; set; } = "";
+
+        [Parameter]
+        public string Title { get; set; } = "";
+
+        [Parameter]
+        public string Message { get; set; } = "";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
