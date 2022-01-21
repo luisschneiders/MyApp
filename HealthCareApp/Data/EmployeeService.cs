7399dc95ad81eecca2730531ffb4e792dcaac3f4
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-21 16:45:37 +1100

    join ContactDetails and Location when displaying employees

--- HealthCareApp/Data/EmployeeService.cs
+++ HealthCareApp/Data/EmployeeService.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using EmployeeLibrary.Models;
-using System.Data;
-using System.Linq;
 using ContactDetailsLibrary.Models;
+using System.Linq;
+using LocationLibrary.Models;
 
 namespace HealthCareApp.Data
 {
@@ -24,15 +24,47 @@ namespace HealthCareApp.Data
         // async method to get list of employees
         public async Task<List<Employee>> GetEmployeesAsync()
         {
-            UserService userService = new (_httpContextAccessor);
+            UserService userService = new UserService(_httpContextAccessor);
+            List<Employee> employeeList = new List<Employee>();
 
-            var employees = await _applicationDbContext.Employees
-                .Include(e => e.ContactDetails)
-                .Where(e => e.InsertedBy == userService.UserId())
-                .OrderByDescending(e => e.CreatedAt)
-                .ToListAsync();
+            /* Raw query with joins, filters and ordering */
+            var query = from employee in _applicationDbContext.Set<Employee>()
+                        join contactDetails in _applicationDbContext.Set<ContactDetails>()
+                            on employee.ContactDetailsId equals contactDetails.Id
+                        join location in _applicationDbContext.Set<Location>()
+                            on employee.LocationId equals location.Id
+                        where employee.InsertedBy == userService.UserId()
+                        orderby employee.CreatedAt descending
+                        select new { employee, contactDetails, location};
 
-            return employees;
+            /* this is not working for some reasons */
+            //var employees = _applicationDbContext.Employees
+            //    .Include(c => c.ContactDetails)
+            //    .Include(l => l.Location)
+            //    .Where(employee => employee.InsertedBy == userService.UserId())
+            //    .OrderByDescending(e => e.CreatedAt)
+            //    .ToList();
+
+            foreach (var i in query)
+            {
+                Employee employee = new Employee();
+                employee = i.employee;
+
+                if (i.employee.ContactDetailsId == i.contactDetails?.Id)
+                {
+                    employee.ContactDetails = i.contactDetails;
+                }
+
+                if (i.employee.LocationId == i.location?.Id)
+                {
+                    employee.Location = i.location;
+                }
+
+                employeeList.Add(employee);
+
+            }
+
+            return await Task.FromResult(employeeList);
         }
 
         // async method to add new employee
