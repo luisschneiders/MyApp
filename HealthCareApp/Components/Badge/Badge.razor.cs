4f3205c5c7a2959376fd43c6b9263ac3d41aad6d
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-24 03:29:43 +1100

    add Badge component

---
+++ HealthCareApp/Components/Badge/Badge.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HealthCareApp.Settings.Enum;
+using HealthCareApp.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.Badge
+{
+    public partial class Badge
+    {
+
+        [Parameter]
+        public Level Level { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        AppSettings AppSettings { get; set; } = new();
+
+        protected string BackgroundColor { get; set; }
+
+        protected override void OnInitialized()
+        {
+            AppSettings.BuildLevel(Level, Message);
+            BackgroundColor = AppSettings.BackgroundColor;
+            Message = AppSettings.Message;
+
+            base.OnInitialized();
+        }
+
+    }
+}
