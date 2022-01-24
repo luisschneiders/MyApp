022b00f835dc803dede41c6b07ff8ba130731872
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-24 14:45:50 +1100

    add class AppSettings

---
+++ HealthCareApp/Shared/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HealthCareApp.Settings.Enum;
+
+namespace HealthCareApp.Shared
+{
+    public class AppSettings
+    {
+        public string BackgroundColor { get; set; }
+
+#nullable enable
+        public string? Message { get; set; }
+
+        public void BuildLevel(Level level, string? message)
+        {
+            switch (level)
+            {
+                case Level.Info:
+                    BackgroundColor = "bg-info";
+                    break;
+                case Level.Warning:
+                    BackgroundColor = "bg-warning";
+                    break;
+                case Level.Error:
+                    BackgroundColor = "bg-danger";
+                    break;
+                case Level.Success:
+                    BackgroundColor = "bg-success";
+                    break;
+            }
+
+            Message = message;
+        }
+    }
+}
