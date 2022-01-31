be86f51b5e3d6de869ebe1f533dcdadbf2699f5c
Luis Schneiders <schneiders.tech@gmail.com>
2022-01-31 22:02:25 +1100

    add new component Spinner

---
+++ HealthCareApp/Components/Spinner/Spinner.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HealthCareApp.Settings.Enum;
+using HealthCareApp.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.Spinner
+{
+    public partial class Spinner
+    {
+        [Parameter]
+        public Size Size { get; set; }
+
+        private AppSettings AppSettings { get; set; } = new();
+
+        private string ComponentSize { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            AppSettings.BuildSize(Size);
+            ComponentSize = AppSettings.ComponentSize;
+        }
+    }
+}
