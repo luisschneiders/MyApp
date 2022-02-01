922d2b73a9f7f3916e25a05b17fca27c4179d621
Luis Schneiders <schneiders.tech@gmail.com>
2022-02-01 17:25:31 +1100

    add page PlaygroundSpinner

---
+++ HealthCareApp/Pages/Playground/PlaygroundSpinner.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using HealthCareApp.Components.Spinner;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.Playground
+{
+    public partial class PlaygroundSpinner
+    {
+        [Inject]
+        SpinnerService SpinnerService { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await Task.Run(() => SpinnerService.ShowSpinner());
+        }
+    }
+}
