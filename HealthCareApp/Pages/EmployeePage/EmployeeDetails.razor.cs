625e1bd797d5603b13d17f788c49fce919da0126
Luis Schneiders <schneiders.tech@gmail.com>
2022-03-01 16:13:42 +1100

    add new component EmployeeDetails

---
+++ HealthCareApp/Pages/EmployeePage/EmployeeDetails.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EmployeeLibrary.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.EmployeePage
+{
+    public partial class EmployeeDetails : ComponentBase
+    {
+
+        [Parameter]
+        public Employee Info { get; set; }
+
+        [Parameter]
+        public bool Loading { get; set; }
+
+    }
+}
