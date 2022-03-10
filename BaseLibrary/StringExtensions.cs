a85ed1b62ffaad826b14d9b38c7dc6a6413a0e84
Luis Schneiders <schneiders.tech@gmail.com>
2022-03-10 20:35:15 +1100

    create extension method Truncate

---
+++ BaseLibrary/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BaseLibrary
+{
+    /*
+     * Syntax for declaring extension methods, a new feature of C# 3.0
+     */
+    public static class StringExtensions
+    {
+        #nullable enable
+        public static string? Truncate(this string? value, int maxLength, string suffix = "...")
+        {
+            return value?.Length > maxLength ? value.Substring(0, maxLength) + suffix : value;
+        }
+    }
+}
