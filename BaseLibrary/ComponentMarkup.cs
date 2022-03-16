ffa9e96bba42460c201d7cadbde21b149004ce9a
Luis Schneiders <schneiders.tech@gmail.com>
2022-03-16 16:05:44 +1100

    add new interface and class ComponentMarkup

---
+++ BaseLibrary/ComponentMarkup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseLibrary
+{
+    public class ComponentMarkup : IComponentMarkup
+    {
+        public string Title { get; set; }
+        public List<string> Code { get; set; }
+    }
+}
---
+++ BaseLibrary/IComponentMarkup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaseLibrary
+{
+    public interface IComponentMarkup
+    {
+        public string Title { get; set; }
+        public List<string> Code { get; set; }
+    }
+}
