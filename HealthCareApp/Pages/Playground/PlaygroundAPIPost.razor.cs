5af57b79aff596bf7579071678501dfc37db98a8
Luis Schneiders <schneiders.tech@gmail.com>
2022-04-22 03:07:42 +1000

    add page PlaygroundAPIPost

---
+++ HealthCareApp/Pages/Playground/PlaygroundAPIPost.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.Playground
+{
+    public partial class PlaygroundAPIPost : ComponentBase
+    {
+        public PlaygroundAPIPost()
+        {
+        }
+    }
+}
