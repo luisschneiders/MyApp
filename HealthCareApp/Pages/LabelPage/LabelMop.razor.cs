259ba5e5c433bf4f119ae7858ede55ef4119f47b
Luis Schneiders <schneiders.tech@gmail.com>
2022-04-27 16:57:57 +1000

    add new page LabelMain and LabelMop; update menu nav; update UI

---
+++ HealthCareApp/Pages/LabelPage/LabelMop.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HealthCareApp.Pages.LabelPage
+{
+    public partial class LabelMop
+    {
+        public LabelMop()
+        {
+        }
+
+        private async Task OpenModalAddAsync()
+        {
+            //await Task.FromResult(EmployeeModalAdd.OpenModalAddAsync());
+            await Task.CompletedTask;
+        }
+    }
+}
