b0a94f3b354ae2ac0da7ac7f6dc8b2e00ee025fe
Luis Schneiders <schneiders.tech@gmail.com>
2022-05-18 20:36:15 +1000

    add model and dto for Area

---
+++ AreaLibrary/Models/Area.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BaseLibrary.Models;
+
+namespace AreaLibrary.Models
+{
+	public class Area : Base
+	{
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        [ForeignKey("Department")]
+        public Guid DepartmentId { get; set; }
+    }
+}
---
+++ AreaLibrary/Models/AreaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AreaLibrary.Models
+{
+	public class AreaDto
+	{
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public Guid DepartmentId { get; set; }
+
+        public string? DepartmentName { get; set; }
+    }
+}
