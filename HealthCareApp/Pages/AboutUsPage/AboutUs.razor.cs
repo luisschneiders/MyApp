9289502b3e0ad2e5d8b3f023f15b8877b509d87e
Luis Schneiders <schneiders.tech@gmail.com>
2022-05-31 13:46:25 +1000

    add new page AboutUs - WIP; update project references

---
+++ HealthCareApp/Pages/AboutUsPage/AboutUs.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.AboutUsPage
+{
+	public partial class AboutUs : ComponentBase
+	{
+		public AboutUs()
+		{
+		}
+	}
+}
+
