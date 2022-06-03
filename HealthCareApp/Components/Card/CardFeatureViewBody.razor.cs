020ea9ce110c75c67814ee4529bc8f9e9dd189a6
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-03 11:31:41 +1000

    add new component CardFeature

---
+++ HealthCareApp/Components/Card/CardFeatureViewBody.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.Card
+{
+	public partial class CardFeatureViewBody : ComponentBase
+	{
+		[Parameter]
+		public RenderFragment? ChildContent { get; set; }
+
+		public CardFeatureViewBody()
+		{
+		}
+	}
+}
+
