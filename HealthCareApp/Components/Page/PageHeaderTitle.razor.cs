6dc5b5efe78efdbe9ff680ebe9027a07b57b2502
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-07 09:42:44 +1000

    add breadcrumb for accessibility; UI updates

--- HealthCareApp/Components/Page/PageHeaderTitle.razor.cs
+++ HealthCareApp/Components/Page/PageHeaderTitle.razor.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using HealthCareApp.Settings.Theme;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace HealthCareApp.Components.Page
 {
     public partial class PageHeaderTitle : ComponentBase
-    {         
+    {
         [Parameter]
-        public string Title { get; set; }
+        public string ParentLink { get; set; }
 
         [Parameter]
-        public Theme Theme { get; set; }
+        public string ParentTitle { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
 
         [Parameter]
         public bool IsVisible { get; set; }
 
         public PageHeaderTitle()
         {
+            ParentLink = "/";
+            ParentTitle = string.Empty;
             Title = string.Empty;
-            Theme = Theme.App;
             IsVisible = true;
         }
 
