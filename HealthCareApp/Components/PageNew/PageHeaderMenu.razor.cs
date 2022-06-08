527185dccd0713540f385637af465ed50831816c
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-08 14:33:34 +1000

    rename files; add new components for Header

---
+++ HealthCareApp/Components/PageNew/PageHeaderMenu.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.PageNew
+{
+	public partial class PageHeaderMenu : ComponentBase
+	{
+		[Parameter]
+		public RenderFragment? ChildContent { get; set; }
+	}
+}
+
---
+++ HealthCareApp/Components/PageNew/PageHeaderTitle.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.PageNew
+{
+	public partial class PageHeaderTitle : ComponentBase
+	{
+
+        [Parameter]
+        public string ParentLink { get; set; }
+
+        [Parameter]
+        public string ParentTitle { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public bool IsVisible { get; set; }
+
+        public PageHeaderTitle()
+		{
+            ParentLink = "/";
+            ParentTitle = string.Empty;
+            Title = string.Empty;
+            IsVisible = true;
+        }
+    }
+}
+
