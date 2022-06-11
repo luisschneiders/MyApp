37d7522d8059457e9b7b6f4a38914968b1047695
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-11 17:43:19 +1000

    add new page TrackingInventory for mops

---
+++ HealthCareApp/Pages/TrackingInventoryPage/TrackingInventoryMopMain.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace HealthCareApp.Pages.TrackingInventoryPage
+{
+	public partial class TrackingInventoryMopMain : ComponentBase
+	{
+		private bool _isInputFocus { get; set; }
+
+		public TrackingInventoryMopMain()
+		{
+            _isInputFocus = false;
+		}
+
+        private async Task OpenModalAsync()
+        {
+
+            await Task.CompletedTask;
+        }
+
+        private async Task SetInputFocusInAsync(FocusEventArgs eventArgs)
+        {
+
+            if (eventArgs?.Type == "focusin")
+            {
+                _isInputFocus = true;
+            }
+
+            await Task.CompletedTask;
+        }
+
+        private async Task SetInputFocusOutAsync(FocusEventArgs eventArgs)
+        {
+
+            if (eventArgs?.Type == "focusout")
+            {
+                _isInputFocus = false;
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
