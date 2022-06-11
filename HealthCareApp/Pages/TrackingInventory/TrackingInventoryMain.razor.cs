d3232b0f7b9169eabdf2f6fb5a1f372817b3bda8
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-11 11:56:44 +1000

    add main tracking inventory page

---
+++ HealthCareApp/Pages/TrackingInventory/TrackingInventoryMain.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.TrackingInventory
+{
+	public partial class TrackingInventoryMain : ComponentBase
+	{
+		public TrackingInventoryMain()
+		{
+		}
+	}
+}
+
