a612abde76c45c549151a1e335399fed80e94b9e
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-17 05:01:14 +1000

    add new library for Tracking Inventory; add models

---
+++ TrackingInventoryLibrary/Models/TrackingInventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BaseLibrary.Models;
+
+namespace TrackingInventoryLibrary.Models
+{
+	public class TrackingInventory : Base
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		public string Barcode { get; set; } = default!;
+
+	}
+}
---
+++ TrackingInventoryLibrary/Models/TrackingInventoryMops.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrackingInventoryLibrary.Models
+{
+	public class TrackingInventoryMops : TrackingInventory
+	{
+
+		[Required]
+		public DateTime TimeIn { get; set; }
+
+		[Required]
+		public DateTime TimeOut { get; set; }
+
+		[Required]
+		[Range(1, 30, ErrorMessage = "Quantity must be between 1 and 30")]
+		public int CleanMopQuantity { get; set; }
+
+		[Required]
+		[Range(1, 30, ErrorMessage = "Quantity must be between 1 and 30")]
+		public int DirtyMopQuantity { get; set; }
+
+		[Required]
+		[ForeignKey("LabelMop")]
+		public Guid LabelMopId { get; set; }
+	}
+}
