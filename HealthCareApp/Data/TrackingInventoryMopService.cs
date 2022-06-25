610c361232993869eebd493d9ee501b71d7b6d28
Luis Schneiders <schneiders.tech@gmail.com>
2022-06-25 16:17:02 +1000

    add new method GetTrackingInventoryMopByDateAsync;

--- HealthCareApp/Data/TrackingInventoryMopService.cs
+++ HealthCareApp/Data/TrackingInventoryMopService.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using AreaLibrary.Models;
 using DateTimeLibrary;
+using DepartmentLibrary.Models;
+using LabelLibrary.Models;
 using Microsoft.EntityFrameworkCore;
 using TrackingInventoryLibrary.Models;
 
@@ -19,11 +22,35 @@ namespace HealthCareApp.Data
         /*
 		 * async method to get mop tracking inventory by date
 		 */
+        public async Task<List<TrackingInventoryMopDto>> GetTrackingInventoryMopByDateAsync(IDateTimeRange dateTime)
+        {
+            UserService userService = new UserService(_httpContextAccessor);
+            List<TrackingInventoryMopDto> trackingInventoryMopDtoList = new();
 
-        //public async Task<TrackingInventoryMopDto> GetBarcodeByDateAsync(IDateTimeRange dateTime)
-        //{
-        //    await Task.CompletedTask;
-        //}
+            /* Raw query with joins, filters and ordering */
+            var query =
+                (
+                    from trackingInventoryMop in _applicationDbContext.Set<TrackingInventoryMop>()
+                    join labelMop in _applicationDbContext.Set<LabelMop>()
+                        on trackingInventoryMop.LabelMopId equals labelMop.Id
+                    join area in _applicationDbContext.Set<Area>()
+                        on labelMop.AreaId equals area.Id
+                    join department in _applicationDbContext.Set<Department>()
+                        on area.DepartmentId equals department.Id
+                    where trackingInventoryMop.InsertedBy == userService.UserId()
+                    && (trackingInventoryMop.PickupTime.Date >= dateTime.Start.Date && trackingInventoryMop.PickupTime.Date <= dateTime.End.Date)
+                    orderby trackingInventoryMop.CreatedAt descending
+                    select new { trackingInventoryMop, labelMop, area, department }
+                ).AsNoTracking();
+
+            foreach (var i in query)
+            {
+                trackingInventoryMopDtoList.Add(SetTrackingInventotyMopDto(i.trackingInventoryMop, i.labelMop, i.area, i.department));
+            }
+
+            return await Task.FromResult(trackingInventoryMopDtoList);
+
+        }
 
         /*
 		 * async method to add tracking inventory for mops
@@ -55,6 +82,35 @@ namespace HealthCareApp.Data
                 Console.WriteLine("Error: {0}", ex.Message);
                 await Task.CompletedTask;
             }
+        }
+
+        private static TrackingInventoryMopDto SetTrackingInventotyMopDto(TrackingInventoryMop trackingInventoryMop, LabelMop labelMop, Area area, Department department)
+        {
+            TrackingInventoryMopDto trackingInventoryMopDto = new();
+            trackingInventoryMopDto.Id = trackingInventoryMop.Id;
+            trackingInventoryMopDto.PickupTime = trackingInventoryMop.PickupTime;
+            trackingInventoryMopDto.ReturnTime = trackingInventoryMop.ReturnTime;
+            trackingInventoryMopDto.CleanMopQuantity = trackingInventoryMop.CleanMopQuantity;
+            trackingInventoryMopDto.DirtyMopQuantity = trackingInventoryMop.DirtyMopQuantity;
+            trackingInventoryMopDto.IsActive = trackingInventoryMop.IsActive;
+
+            if (trackingInventoryMop.LabelMopId == labelMop?.Id)
+            {
+                trackingInventoryMopDto.Barcode = labelMop.Barcode;
+                trackingInventoryMopDto.MopQuantity = labelMop.Quantity;
+            }
+
+            if (labelMop?.AreaId == area?.Id)
+            {
+                trackingInventoryMopDto.AreaName = area?.Name;
+            }
+
+            if (department?.Id == area?.DepartmentId)
+            {
+                trackingInventoryMopDto.DepartmentName = department?.Name;
+            }
+
+            return trackingInventoryMopDto;
         }
     }
 }
