6c495ff20353ee58bb0053d074c98d451c8fcd5c
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-01 07:50:28 +1000

    code refactoring; UI updates

--- HealthCareApp/Shared/NavMenu.razor.cs
+++ HealthCareApp/Shared/NavMenu.razor.cs
@@ -14,38 +14,38 @@ namespace HealthCareApp.Shared
             Api
         };
 
-        private bool _collapseNavMenu { get; set; }
-        private bool _collapseNavSubmenu { get; set; }
+        //private bool _collapseNavMenu { get; set; }
+        //private bool _collapseNavSubmenu { get; set; }
 
-        private NavSubmenu _navSubmenu { get; set; }
+        //private NavSubmenu _navSubmenu { get; set; }
 
-        private string _navMenuCssClass => _collapseNavMenu ? "collapse" : string.Empty;
+        //private string _navMenuCssClass => _collapseNavMenu ? "collapse" : string.Empty;
 
         public NavMenu()
         {
-            _collapseNavMenu = true;
-            _collapseNavSubmenu = true;
-            _navSubmenu = NavSubmenu.None;
+            //_collapseNavMenu = true;
+            //_collapseNavSubmenu = true;
+            //_navSubmenu = NavSubmenu.None;
         }
 
 
-        private void ToggleNavMenu()
-        {
-            _collapseNavMenu = !_collapseNavMenu;
-        }
+        //private void ToggleNavMenu()
+        //{
+        //    _collapseNavMenu = !_collapseNavMenu;
+        //}
 
-        private void ToggleNavSubmenu(NavSubmenu submenu)
-        {
-            _collapseNavSubmenu = !_collapseNavSubmenu;
+        //private void ToggleNavSubmenu(NavSubmenu submenu)
+        //{
+        //    _collapseNavSubmenu = !_collapseNavSubmenu;
 
-            if (_navSubmenu == submenu)
-            {
-                _navSubmenu = NavSubmenu.None;
-            }
-            else
-            {
-                _navSubmenu = submenu;
-            }
-        }
+        //    if (_navSubmenu == submenu)
+        //    {
+        //        _navSubmenu = NavSubmenu.None;
+        //    }
+        //    else
+        //    {
+        //        _navSubmenu = submenu;
+        //    }
+        //}
     }
 }
