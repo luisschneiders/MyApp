6dee5dc0c5c9bf235efbfa8d40819a078e3cb28c
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-01 07:41:59 +1000

    add new component LoginOrRegister

---
+++ HealthCareApp/Shared/LoginOrRegister.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Shared
+{
+	public partial class LoginOrRegister : ComponentBase
+	{
+		public LoginOrRegister()
+		{
+		}
+	}
+}
