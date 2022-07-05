4ccd3bef56bf96839d996b0909c5cd775ccd8218
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-05 10:23:35 +1000

    add new component OffCanvas

---
+++ HealthCareApp/Components/OffCanvas/OffCanvas.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.OffCanvas
+{
+	public partial class OffCanvas : ComponentBase
+	{
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        // start or end
+        [Parameter]
+		public string Position { get; set; } = default!;
+
+        // offcanvasLeft
+        [Parameter]
+        public string Id { get; set; } = default!;
+
+        // offcanvasLeftLabel
+        [Parameter]
+        public string LabelId { get; set; } = default!;
+
+        // sm, md, lg
+        [Parameter]
+        public string Size { get; set; } = default!;
+
+        public OffCanvas()
+		{
+            Position = "end";
+            Id = "offcanvasRight";
+            LabelId = "offcanvasRightLabel";
+        }
+	}
+}
