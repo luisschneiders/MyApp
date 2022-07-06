0e6c18003be16f9874163f2e6f8a5ce72dd6d2e8
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-06 16:52:22 +1000

    add component CardProfile

---
+++ HealthCareApp/Components/CardV3/CardProfile.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.CardV3
+{
+	public partial class CardProfile : ComponentBase
+	{
+        [Parameter]
+		public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+		public string CardProfileIcon { get; set; }
+
+        [Parameter]
+		public string CardProfileTitle { get; set; }
+
+		public CardProfile()
+		{
+            CardProfileIcon = string.Empty;
+			CardProfileTitle = string.Empty;
+		}
+	}
+}
