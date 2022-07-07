d6f82124108f27767c3eeb3ecb0c311d6fde06e8
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-07 11:36:04 +1000

    refactor page PlaygroundSpinner UI

--- HealthCareApp/Pages/PlaygroundPage/PlaygroundSpinner.razor.cs
+++ HealthCareApp/Pages/PlaygroundPage/PlaygroundSpinner.razor.cs
@@ -8,9 +8,127 @@ namespace HealthCareApp.Pages.PlaygroundPage
         [Inject]
         private SpinnerService _spinnerService { get; set; }
 
+        private ComponentMarkup _componentMarkup { get; set; }
+        private List<ComponentMarkup> _componentMarkupList { get; set; }
+        private List<string> _codes { get; set; }
+        private List<bool> _newLine { get; set; }
+        private List<string> _cssStyle { get; set; }
+
         public PlaygroundSpinner()
         {
             _spinnerService = new();
+            _componentMarkup = new();
+            _componentMarkupList = new();
+            _codes = new List<string>();
+            _cssStyle = new List<string>();
+            _newLine = new List<bool>();
+        }
+
+        protected override void OnInitialized()
+        {
+
+            _codes = new()
+            {
+                new MarkupString("<Spinner />").ToString(),
+            };
+
+            _cssStyle = new()
+            {
+                "",
+            };
+
+            _newLine = new()
+            {
+                false,
+            };
+
+            _componentMarkup = new()
+            {
+                Title = "Spinner",
+                Code = _codes,
+                NewLine = _newLine,
+                CssStyle = _cssStyle
+            };
+            _componentMarkupList.Add(_componentMarkup);
+
+            _codes = new()
+            {
+                new MarkupString("Size='' ").ToString(),
+            };
+
+            _cssStyle = new()
+            {
+                "",
+            };
+
+            _newLine = new()
+            {
+                false,
+            };
+
+            _componentMarkup = new()
+            {
+                Title = "Parameters",
+                Code = _codes,
+                NewLine = _newLine,
+                CssStyle = _cssStyle
+            };
+            _componentMarkupList.Add(_componentMarkup);
+
+            _codes = new()
+            {
+                new MarkupString("SpinnerService.ShowSpinner();").ToString(),
+                new MarkupString("SpinnerService.HideSpinner();").ToString(),
+            };
+
+            _cssStyle = new()
+            {
+                "",
+                "",
+            };
+
+            _newLine = new()
+            {
+                false,
+                false,
+            };
+
+            _componentMarkup = new()
+            {
+                Title = "Service",
+                Code = _codes,
+                NewLine = _newLine,
+                CssStyle = _cssStyle
+            };
+            _componentMarkupList.Add(_componentMarkup);
+
+            _codes = new()
+            {
+                new MarkupString("ShowSpinner()").ToString(),
+                new MarkupString("HideSpinner()").ToString(),
+            };
+
+            _cssStyle = new()
+            {
+                "",
+                "",
+            };
+
+            _newLine = new()
+            {
+                false,
+                false,
+            };
+
+            _componentMarkup = new()
+            {
+                Title = "Method",
+                Code = _codes,
+                NewLine = _newLine,
+                CssStyle = _cssStyle
+            };
+            _componentMarkupList.Add(_componentMarkup);
+
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
