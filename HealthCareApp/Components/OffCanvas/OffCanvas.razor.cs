aa6c950bc2112a723fcb6e09b5ea379750c74840
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-08 18:06:07 +1000

    more code refactoring

--- HealthCareApp/Components/OffCanvas/OffCanvas.razor.cs
+++ HealthCareApp/Components/OffCanvas/OffCanvas.razor.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using HealthCareApp.Settings.Enum;
 
 namespace HealthCareApp.Components.OffCanvas
 {
@@ -11,25 +11,41 @@ namespace HealthCareApp.Components.OffCanvas
 
         // start or end
         [Parameter]
-		public string Position { get; set; } = default!;
+		public string Position { get; set; }
 
-        // offcanvasLeft
         [Parameter]
-        public string Id { get; set; } = default!;
+        public OffCanvasSize Size { get; set; }
 
-        // offcanvasLeftLabel
-        [Parameter]
-        public string LabelId { get; set; } = default!;
-
-        // sm, md, lg
-        [Parameter]
-        public string Size { get; set; } = default!;
+        private Guid _offCanvasId { get; set; }
+        private string _offCanvasClass { get; set; }
+        private bool _showBackdrop { get; set; }
 
         public OffCanvas()
 		{
             Position = "end";
-            Id = "offcanvasRight";
-            LabelId = "offcanvasRightLabel";
+            _showBackdrop = false;
+            _offCanvasId = Guid.Empty;
         }
-	}
+
+        public async Task Open(Guid target)
+        {
+
+            _offCanvasId = target;
+            await Task.Delay((int)Delay.OffCanvasOpen);
+            _offCanvasClass = "show";
+            _showBackdrop = true;
+
+            StateHasChanged();
+        }
+
+        public async Task Close(Guid target)
+        {
+            _offCanvasId = target;
+            _offCanvasClass = string.Empty;
+            await Task.Delay((int)Delay.OffCanvasClose);
+            _showBackdrop = false;
+
+            StateHasChanged();
+        }
+    }
 }
