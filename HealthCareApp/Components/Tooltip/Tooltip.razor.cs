cd5190ccce84a95ef6c6ff58053ebbbb9abe14e2
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-12 15:18:02 +1000

    create new component Popover and Tooltip

---
+++ HealthCareApp/Components/Tooltip/Tooltip.razor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using HealthCareApp.Settings.Enum;
+using HealthCareApp.Settings.Theme;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace HealthCareApp.Components.Tooltip
+{
+	public partial class Tooltip : ComponentBase
+	{
+        [Parameter]
+		public Position Position { get; set; }
+
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+		public Theme BackgroundColor { get; set; }
+
+        private IJSObjectReference? _module;
+
+        public Tooltip()
+		{
+			Position = Position.top;
+			BackgroundColor = Theme.light;
+			Title = string.Empty;
+		}
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _module = await JS.InvokeAsync<IJSObjectReference>("import",
+                    "./Components/Tooltip/Tooltip.razor.js");
+            }
+            await Task.CompletedTask;
+
+        }
+
+        /*
+         * async dispose module
+         */
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            if (_module is not null)
+            {
+                await _module.DisposeAsync();
+            }
+        }
+    }
+}
