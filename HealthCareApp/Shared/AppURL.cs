0d5fb7b47d5ab49000dd5c47da481735852af40a
Luis Schneiders <schneiders.tech@gmail.com>
2022-07-25 12:42:12 +1000

    update urls used in the app

--- HealthCareApp/Shared/AppURL.cs
+++ HealthCareApp/Shared/AppURL.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using HealthCareApp.Pages.PlaygroundPage;
+
 namespace HealthCareApp.Shared
 {
     public class AppURL
@@ -22,6 +24,12 @@ namespace HealthCareApp.Shared
 
         public readonly string Playground;
         public readonly string PlaygroundCard;
+        public readonly string PlaygroundCardV3;
+        public readonly string PlaygroundPageV3;
+        public readonly string PlaygroundModal;
+        public readonly string PlaygroundToast;
+        public readonly string PlaygroundBadge;
+        public readonly string PlaygroundSpinner;
 
         public readonly string Tasks;
         public readonly string TasksHistoryMop;
@@ -47,6 +55,12 @@ namespace HealthCareApp.Shared
 
             Playground = "/playground";
             PlaygroundCard = $"{Playground}/card";
+            PlaygroundCardV3 = $"{Playground}/card-v3";
+            PlaygroundPageV3 = $"{Playground}/page-v3";
+            PlaygroundModal = $"{Playground}/modal";
+            PlaygroundToast = $"{Playground}/toast";
+            PlaygroundBadge = $"{Playground}/badge";
+            PlaygroundSpinner = $"{Playground}/spinner";
 
             Tasks = "/tasks";
             TasksHistoryMop = $"{Tasks}/history-mop";
