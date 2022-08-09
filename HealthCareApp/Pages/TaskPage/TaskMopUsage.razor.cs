b995999282115d373026a04eebe165d7e159faf3
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-09 17:26:08 +1000

    add new pages; delete old pages;

---
+++ HealthCareApp/Pages/TaskPage/TaskMopUsage.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HealthCareApp.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.TaskPage
+{
+    public partial class TaskMopUsage : ComponentBase
+    {
+        private AppURL _appURL { get; }
+
+        public TaskMopUsage()
+        {
+            _appURL = new();
+        }
+    }
+}
