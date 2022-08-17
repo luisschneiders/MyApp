919facf474abf25614c7c6c5dbfb0c7a21f20ca0
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-17 13:28:37 +1000

    add method CheckDate for DateTimeRange class

--- DateTimeLibrary/DateTimeRange.cs
+++ DateTimeLibrary/DateTimeRange.cs
@@ -1,13 +1,18 @@
-﻿using System;
-namespace DateTimeLibrary
+﻿namespace DateTimeLibrary
 {
 	public class DateTimeRange : IDateTimeRange
 	{
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
 
+
         public DateTimeRange()
 		{
 		}
-	}
+
+        public bool CheckDate()
+        {
+            return Start.Date <= End.Date;
+        }
+    }
 }
--- DateTimeLibrary/IDateTimeRange.cs
+++ DateTimeLibrary/IDateTimeRange.cs
@@ -5,5 +5,7 @@ namespace DateTimeLibrary
 	{
 		public DateTime Start { get; set; }
 		public DateTime End { get; set; }
+
+		public bool CheckDate();
 	}
 }
