5c83aa716e4c2b85697185dadab7d165bd19ae72
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-19 13:26:03 +1000

    add new component DropdownDateRange

---
+++ HealthCareApp/Components/Dropdown/DropdownDateRange.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DateTimeLibrary;
+using HealthCareApp.Settings.Enum;
+using HealthCareApp.Settings.Theme;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Components.Dropdown
+{
+	public partial class DropdownDateRange : ComponentBase
+	{
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public bool IsDisplayLargeNone { get; set; }
+
+        [Parameter]
+        public Theme IconColor { get; set; }
+
+        [Parameter]
+        public Theme ButtonColor { get; set; }
+
+        [Parameter]
+        public Size ButtonSize { get; set; }
+
+        [Parameter]
+        public string DateRange { get; set; }
+
+        private IDateTimeRange _dateTimeRange { get; set; }
+
+        public DropdownDateRange()
+		{
+            IsDisplayLargeNone = false;
+            ButtonColor = Theme.light;
+            IconColor = Theme.success;
+            ButtonSize = Size.md;
+            _dateTimeRange = new DateTimeRange
+            {
+                Start = DateTime.Now,
+                End = DateTime.Now
+            };
+            DateRange = $"{_dateTimeRange.Start.Date.ToString("dd/MM/yy")} - {_dateTimeRange.End.Date.ToString("dd/MM/yy")}";
+        }
+	}
+}
