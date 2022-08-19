5f246a69481f28f9660bc3f712482f3883d9c704
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-19 10:18:43 +1000

    update dashboard; add new component MopUsage;

---
+++ HealthCareApp/Pages/DashboardPage/MopUsage.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HealthCareApp.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace HealthCareApp.Pages.DashboardPage
+{
+	public partial class MopUsage : ComponentBase
+	{
+
+        public MopUsage()
+		{
+		}
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
