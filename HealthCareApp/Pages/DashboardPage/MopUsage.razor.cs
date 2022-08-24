4faf7f879ec3606beec3ac561f0f924dc2fcfcc2
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-24 12:12:12 +1000

    code updates

--- HealthCareApp/Pages/DashboardPage/MopUsage.razor.cs
+++ HealthCareApp/Pages/DashboardPage/MopUsage.razor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using HealthCareApp.Components.Chart;
 using HealthCareApp.Data;
+using HealthCareApp.Settings.Enum;
 using Microsoft.AspNetCore.Components;
 
 namespace HealthCareApp.Pages.DashboardPage
@@ -7,9 +9,32 @@ namespace HealthCareApp.Pages.DashboardPage
 	public partial class MopUsage : ComponentBase
 	{
 
+        private List<string> _chartBackgroundColors { get; set; }
+        private List<string> _chartBorderColors { get; set; }
+        private List<string> _chartLabels { get; set; }
+
         public MopUsage()
 		{
-		}
+            _chartBackgroundColors = new()
+            {
+                BackgroundColor.Red,
+                BackgroundColor.Yellow,
+                BackgroundColor.Green
+
+            };
+            _chartBorderColors = new()
+            {
+                BorderColor.Red,
+                BorderColor.Yellow,
+                BorderColor.Green
+            };
+            _chartLabels = new()
+            {
+                MopStatus.Missing.ToString(),
+                MopStatus.Dirty.ToString(),
+                MopStatus.Clean.ToString(),
+            };
+        }
 
         protected async override Task OnInitializedAsync()
         {
