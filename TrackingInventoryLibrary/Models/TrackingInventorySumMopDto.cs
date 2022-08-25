61b81e0a772ffd2c98829481b8ad33ae4b25a6eb
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-26 08:38:41 +1000

    add new DTO model for TrackingInventoryMop

---
+++ TrackingInventoryLibrary/Models/TrackingInventorySumMopDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TrackingInventoryLibrary.Models
+{
+	public class TrackingInventorySumMopDto
+	{
+        public DateTime ScanTime { get; set; }
+
+        public int CleanMopQuantity { get; set; }
+
+        public int DirtyMopQuantity { get; set; }
+
+        public int MopQuantity { get; set; }
+    }
+}
