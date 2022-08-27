04f0f53bb10b0a3b0c26c23f5d38ca7985b78b7e
Luis Schneiders <schneiders.tech@gmail.com>
2022-08-27 15:03:23 +1000

    add chart js file into Chart component folder

---
+++ HealthCareApp/Components/Chart/Chart.razor.js
@@ -0,0 +1,23 @@
+﻿let chart = {};
+
+export function setupChart(chartId, config) {
+    const ctx = document.getElementById(chartId).getContext('2d');
+
+    chart = new Chart(ctx, config);
+}
+
+export function updateChartData(data) {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = data;
+    });
+
+    chart.update();
+}
+
+export function removeChartData() {
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+
+    chart.update();
+}
