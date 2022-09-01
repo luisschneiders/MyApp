704acee0f53148c130e8ba7a9fe357d9d7e7b740
Luis Schneiders <schneiders.tech@gmail.com>
2022-09-01 10:12:15 +1000

    add new page for account management

---
+++ HealthCareApp/Areas/Identity/Pages/Account/Manage.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using MyApp.Shared;
+
+namespace MyApp.Areas.Identity.Pages.Account
+{
+	public partial class Manage : ComponentBase
+	{
+        private AppURL _appURL { get; }
+
+        public Manage()
+		{
+            _appURL = new();
+        }
+	}
+}
